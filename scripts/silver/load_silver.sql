e5957de72cfc6f232f5caedf38f231373185a9d1
arun20kalyan <87612755+arun20kalyan@users.noreply.github.com>
2025-06-23 21:39:21 -0500

    Update load_silver.sql

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -59,3 +59,70 @@ select
 	dateadd(DAY,-1,lead(prd_start_dt) over(partition by prd_key order by prd_start_dt)) as prd_end_dt
 from bronze.crm_prd_info;
 
+
+
+insert into silver.crm_sales_details(
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+)
+select 
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	case when sls_order_dt  = 0 or len(sls_order_dt) != 8 then null
+		else cast(cast(sls_order_dt as varchar) as date) end sls_order_dt,
+	case when sls_ship_dt  = 0 or len(sls_ship_dt) != 8 then null
+		else cast(cast(sls_ship_dt as varchar) as date) end sls_ship_dt,
+	case when sls_due_dt  = 0 or len(sls_due_dt) != 8 then null
+		else cast(cast(sls_due_dt as varchar) as date) end sls_due_dt,
+	case when sls_sales is null or sls_sales <= 0 or sls_sales != sls_quantity * abs(sls_price)
+			then sls_quantity * abs(sls_price)
+		else sls_sales
+	end sls_sales,
+	sls_quantity,
+	case when sls_price is null or sls_price <= 0
+			then sls_sales / nullif(sls_quantity, 0)
+		else sls_price
+	end sls_price
+from bronze.crm_sales_details;
+
+
+
+
+insert into silver.erp_cust_az12(
+	cid,
+	bdate,
+	gen)
+select 
+	case when cid like 'NAS%' then substring(cid, 4, len(cid))
+		else cid
+	end cid,
+	case when bdate > getdate() then null
+		else bdate
+	end bdate,
+	case when upper(trim(gen)) in ('M', 'Male') then 'Male'
+		when upper(trim(gen)) in ('F', 'Female') then 'Female'
+		else 'n/a'
+	end gen
+from bronze.erp_cust_az12;
+
+
+insert into silver.erp_loc_a101(
+	cid,
+	cntry
+)
+select
+	REPLACE(cid,'-','') cid,
+	case when trim(cntry) = 'DE' then 'Germany'
+		when trim(cntry) in ('US', 'USA') then 'United States'
+		when  trim(cntry) = '' or cntry is null then 'n/a'
+		else trim(cntry)
+	end cntry
+from bronze.erp_loc_a101;
